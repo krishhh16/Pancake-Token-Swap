2f267ae5234c64dbadf1413f4c9831c999ba2bec
krishh16 <krishh16@yahoo.com>
2024-01-13 14:42:11 +0530

    added info

---
+++ AddressList.js
@@ -0,0 +1,11 @@
+const factoryAddress = "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73";
+const routerAddress = "";
+const fromAddress = "";
+const toAddress = "";
+
+module.exports = {
+  factoryAddress,
+  routerAddress,
+  fromAddress,
+  toAddress
+}
